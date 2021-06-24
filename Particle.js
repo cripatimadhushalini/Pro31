67d61096ca2160c41808f88a31466625150b60ac
cripatimadhushalini <78367239+cripatimadhushalini@users.noreply.github.com>
2021-06-24 10:50:12 +0530

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,35 @@
+class Particle {
+    constructor(x, y) {
+        var options = {
+            restitution: 1,
+            friction: 0,
+            isStatic:false,
+        }
+
+        this.r = 10;
+        this.part = Bodies.circle(x, y, this.r, options);
+        this.part = Bodies.circle(x,y,this.r,options);
+        this.color = color(random(0,255),random(0,255),random(0,255));
+        World.add(world, this.part);
+
+    }
+
+
+    display() {
+
+        var pos = this.part.position;
+        var angle = this.part.angle;
+
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        //imageMode(CENTER);
+       // noStroke();
+        //fill("black");
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop();
+        
+    }
+
+};
